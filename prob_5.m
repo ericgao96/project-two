53a690bfa0c43b314491be2393ccb88b46996fa1
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-03-10 22:01:13 -0800

    Update prob_5.m

--- prob_5.m
+++ prob_5.m
@@ -1,3 +1,4 @@
-function weights=prob_5(n)      % initialize the network by generating a random weight matrix
+function [weights,weights2]=prob_5(n)      % initialize the network by generating a random weight matrix
     weights=rand(n,n);
+    weights2=rand(n,10);
 end
