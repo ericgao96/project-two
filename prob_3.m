ee4bdc7a1ea93c66fdb89ab87e8cac61e298132e
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-03-10 17:05:58 -0800

    Update prob_3.m

--- prob_3.m
+++ prob_3.m
@@ -1,4 +1,4 @@
 function neuron=prob_3(connection, weight)         % connection and weight are both n-vector
-  NET=connection*weight';                          % NET = sum of all pairs of (Oi,Wi)
+  NET=weight'*connection;                          % NET = sum of all pairs of (Oi,Wi)
   neuron=1/(1+exp(-NET));
 end
