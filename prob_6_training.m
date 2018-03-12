ccf50ec21dfc2f744a0e7fed56e747a0d444fb37
liubangdi <35050496+liubangdi@users.noreply.github.com>
2018-03-11 19:21:28 -0700

    Update prob_6_training.m

--- prob_6_training.m
+++ prob_6_training.m
@@ -1,4 +1,4 @@
-function w=prob_6_training(y,outputs, output,target,weights2)
+function w=prob_6_training(y,outputs, output,target,weights,numhidLayers,numneurons,input)
 %Adjusting the weights of the output layer. 
 %y is yita(training rate, 0.01~0.1)
 %outputs is input layer and hidden layer outputs.
@@ -7,14 +7,24 @@ function w=prob_6_training(y,outputs, output,target,weights2)
 %weights weight coefficient matrix of hidden layers.
 %weights is weight coefficeint matrix between last hidden layer and output
 %layer.
+input=input(:);
 [n,m]=size(outputs);
+error=abs(output-target);
+%target and output are column vectors
+delta={ones(numneurons,numhidLayers),[]};  %we only need delta for hidlayers and outputlayers
+delta{2}=output.*(1-output).*error;
+weights{numhidLayers+1}=weights{numhidLayers+1}+y*outputs(numhidLayers)*delta{2}';
 
-for q=1:10
-    error=abs(target(q)-output(q));
-    delta=output(q)*(1-output(q))*error;
-    for p=1:n
-        weights2(p,q)=weights2(p,q)+y*delta*outputs(p,end);
+for j=numhidLayers:-1:2
+    if j==numhidLayers
+        delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{2});
+    else
+        delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{1}(:,j+1));
     end
+    weights{j}=weights{j}+y*delta{1}(:,j)*outputs(:,j-1)';
 end
-w=weights2;
-end
+j=1;
+delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{1}(:,j+1));
+weights{j}=weights{j}+y*input*delta{1}(:,j)'; 
+    
+    
