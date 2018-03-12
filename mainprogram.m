c856be55f76974b8cce9ad9eb9c5a38abd37a9f4
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-03-12 14:47:21 -0700

    Update mainprogram.m

--- mainprogram.m
+++ mainprogram.m
@@ -8,7 +8,7 @@ prob_2;
 % set up training parameters
 numhidLayers=input('Enter the number of hidden layers: ');
 numNeurons=input('Enter the number of neurons per hidden layer: ');
-yita=0.05;
+yita=input('Enther the training rate coefficient: ');
 % initialize weight matrix
 weights=prob_5_initialize(numhidLayers,numNeurons);
 % intialize target matrix
@@ -18,10 +18,9 @@ for i=1:10
 end
 
 % first training using digit '0'
-[output,outputs]=prob_4_network(numNeurons,numhidLayers,trainingset{1}(:),weights);
-weights=prob_6_training(yita,outputs,output,target(1,:)',weights,numhidLayers,numNeurons,trainingset{1});
-% continue training using digit '1'-'9' if necessary
-for i=2:10
-    [output,outputs]=prob_4_network(numNeurons,numhidLayers,trainingset{i}(:),weights);
-    weights=prob_6_training(yita,outputs,output,target(i,:)',weights,numhidLayers,numNeurons,trainingset{i});
+for j=1:1000
+    for i=1:10
+        [output,outputs]=prob_4_network(numNeurons,numhidLayers,trainingset{i}(:),weights);
+        weights=prob_6_training(yita,outputs,output,target(:,i),weights,numhidLayers,numNeurons,trainingset{i});
+    end
 end
