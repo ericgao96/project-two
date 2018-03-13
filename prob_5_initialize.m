b963d5ae7689797da9c942c142bdd72ffc0f8c4e
liubangdi <35050496+liubangdi@users.noreply.github.com>
2018-03-12 18:10:39 -0700

    Update prob_5_initialize.m

--- prob_5_initialize.m
+++ prob_5_initialize.m
@@ -4,8 +4,8 @@ function weights=prob_5_initialize(numhidLayers,numNeurons)
 %numNuerons can be arbitrary constant.
 weights={rand(784,numNeurons)};
 for i=2:numhidLayers
-    weights{i}=rand(numNeurons,numNeurons);
+    weights{i}=rand(numNeurons,numNeurons)/1000;
 end
-weights{numhidLayers+1}=rand(numNeurons,10); 
+weights{numhidLayers+1}=rand(numNeurons,10)/1000; 
 
 end
