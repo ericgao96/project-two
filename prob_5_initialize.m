151e9dd2dd26ec6a70651bc18c6fd2b6b67dbb9c
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-03-12 18:17:52 -0700

    Update prob_5_initialize.m

--- prob_5_initialize.m
+++ prob_5_initialize.m
@@ -1,7 +1,9 @@
 function weights=prob_5_initialize(numhidLayers,numNeurons)
-%784 is dimension of input layers, 10 is dimension of output layers,which
-%are fixed. 
-%numNuerons can be arbitrary constant.
+% 784 is dimension of input layers, 10 is dimension of output layers,which
+% are fixed. 
+% numNuerons can be arbitrary constant.
+% weights is a cell consists of L+1 weight matrix.1 of them is 784*numNeurons;L of them is numNeurons*numNeurons;1
+% of them is numNeurons*10;
 weights={rand(784,numNeurons)};
 for i=2:numhidLayers
     weights{i}=rand(numNeurons,numNeurons)/1000;
