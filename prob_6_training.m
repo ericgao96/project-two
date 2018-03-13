dbe83f5048a3b9fe5fd0f00b4dea6245fd87e2f6
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-03-13 16:38:19 -0700

    Update prob_6_training.m

--- prob_6_training.m
+++ prob_6_training.m
@@ -4,24 +4,24 @@ function W=prob_6_training(y,outputs, output,target,weights,numhidLayers,numNeur
 %outputs is input layer and hidden layer outputs.
 %output is output layer.
 %target is the target values. Ex.Digit 1 = (0 1 0 0 0 0 0 0 0 0)
-%weights weight coefficient matrix of hidden layers.
-%weights is weight coefficeint matrix between last hidden layer and output
+%weights is the weight coefficient matrix of hidden layers.
+%weights is the weight coefficeint matrix between last hidden layer and output
 %layer.
 
 input=input(:);
-error=target-output;
+error=target-output;      
 %target and output are column vectors
 delta={ones(numNeurons,numhidLayers),[]};  %we only need delta for hidlayers and outputlayers
-delta{2}=output.*(1-output).*error;
+delta{2}=output.*(1-output).*error;       %delta{2}=output.*(1-output).*error;
 weights{numhidLayers+1}=weights{numhidLayers+1}+y*outputs(numhidLayers)*delta{2}';
 
-for j=numhidLayers:-1:2
+for j=numhidLayers:-1:2  
     if j==numhidLayers
         delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{2});
     else
         delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{1}(:,j+1));
     end
-    weights{j}=weights{j}+y*delta{1}(:,j)*outputs(:,j-1)';
+    weights{j}=weights{j}+y*delta{1}(:,j)*outputs(:,j-1)';    % this needs to be checked. Try:weights{j}=weights{j}+y*outputs(:,j-1)*delta{1}(:,j)';
 end
 j=1;
 delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{1}(:,j+1));
