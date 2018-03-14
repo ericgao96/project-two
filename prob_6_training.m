5da9f211f718d5bdf5e8aba9fe3a5d14b23483dc
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-03-13 19:09:41 -0700

    Update prob_6_training.m

--- prob_6_training.m
+++ prob_6_training.m
@@ -13,17 +13,17 @@ error=target-output;
 %target and output are column vectors
 delta={ones(numNeurons,numhidLayers),[]};  %we only need delta for hidlayers and outputlayers
 delta{2}=output.*(1-output).*error;       %delta{2}=output.*(1-output).*error;
-weights{numhidLayers+1}=weights{numhidLayers+1}+y*outputs(numhidLayers)*delta{2}';
+weights{numhidLayers+1}=weights{numhidLayers+1}+y*double(outputs(numhidLayers))*double(delta{2}');
 
 for j=numhidLayers:-1:2  
     if j==numhidLayers
-        delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{2});
+        delta{1}(:,j)=double(outputs(:,j)).*double((1-outputs(:,j))).*double((weights{1+j})*double(delta{2}));
     else
-        delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{1}(:,j+1));
+        delta{1}(:,j)=double(outputs(:,j)).*double((1-outputs(:,j))).*double((weights{1+j})*double(delta{1}(:,j+1)));
     end
-    weights{j}=weights{j}+y*delta{1}(:,j)*outputs(:,j-1)';    % this needs to be checked. Try:weights{j}=weights{j}+y*outputs(:,j-1)*delta{1}(:,j)';
+    weights{j}=weights{j}+y*double(outputs(:,j-1))*double(delta{1}(:,j)');  % this needs to be checked. Try:weights{j}=weights{j}+y*outputs(:,j-1)*delta{1}(:,j)';original: weights{j}=weights{j}+y*delta{1}(:,j)*outputs(:,j-1)'
 end
 j=1;
-delta{1}(:,j)=outputs(:,j).*(1-outputs(:,j)).*(weights{1+j}*delta{1}(:,j+1));
+delta{1}(:,j)=double(outputs(:,j)).*double((1-outputs(:,j))).*double((weights{1+j})*double(delta{1}(:,j+1)));
 weights{j}=weights{j}+y*double(input)*double(delta{1}(:,j)'); 
 W=weights;
